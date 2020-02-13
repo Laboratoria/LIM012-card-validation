de189466e9c8542523371bd572027b6d0fa15b60
luzma-coder <luz.marina.acdr@gmail.com>
2020-02-13 12:32:38 -0500

    luz.marina.acdr@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+// declaracion 
+const datos=document.getElementById('datos');
+const resultado=document.getElementById('resultado');
+const mensaje=document.getElementById('mensaje');
+const btnsend=document.getElementById('btnsend');
+const creditCardNumber=getElementById('cardcli');
+
+// primera pantalla
+datos.style.display='block';
+resultado.style.display='none';
+mensaje.innerHTML= 'Gracias por elegirnos';
+
+// evaluar tarjeta
+
+const cardValid = () => {
+    let respuesta=Boolean;
+    datos.style.display='none';
+    resultado.style.display='block';
+    respuesta=isValid(creditCardNumber);
+    mensaje.innerHTML= 'Comprobante y muestra tarjeta XXXXXX9999';    
+};
+//onclick
+
+btnsend.addEventListener('click', cardValid);
+
+//document.getElementById('btnsend').addEventListener('click',validar);
+
+ //btnsend.onclick = function(){
+//    datos.style.display='none';
+//    resultado.style.display='block';
+//    mensaje.innerHTML= 'Comprobante y muestra tarjeta XXXXXX9999';    
+//  };
+
+  
+  
+  
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
   // ...
+  arrcardnumbers:[],
+
+  isValid: function(creditCardNumber){
+    // invierto el arreglo en arrcardnumbers ya seran numeros
+    let old=15;
+    let producto=0;
+    let caracter;
+    let suma=0;
+    for (let i = 0; i <= 15; i = i + 1) {
+      arrcardnumbers[i]= creditCardNumber[old]*1;
+      old=old-1;
+     } 
+    //multiplico x 2 las ubicaciones pares
+    for (let j = 1; j <= 15; j = j + 2) {
+      producto=arrcardnumbers[i]*2;
+      if (producto>9){
+        caracter=producto-10;
+        arrcardnumbers[i]=(caracter*1)+1;
+      }
+    }
+    // sumo a todos
+    for (let i = 0; i <= 15; i = i + 1) {
+      suma = suma+arrcardnumbers[i];
+    }  
+    if (suma%10){
+      return true;
+    else 
+      return false;
+    }
+  }
+};
+
+
+
+
+const F1Race = {
+  drivers: [
+    'Alonso',
+    'Vettel',
+    'Button',
+    'Massa'
+  ],
+  init: function () {
+    console.log('Los siguientes pilotos van a comenzar la carrera:', this.drivers);
+    setTimeout((function () {
+      console.log('El ganador es', randomWinner(this.drivers));
+    }), 1000);
+  }
 };
 
 export default validator;
