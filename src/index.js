ddb6be0aa3e533a13fe996623b26761ac25e7a80
katherinChi <kathychi02129@gmail.com>
2020-02-20 11:04:15 -0500

    20 febrero 2020

--- src/index.js
+++ src/index.js
@@ -1,3 +1,82 @@
 import validator from './validator.js';
+//import {isValid} from './validator.js';
 
-console.log(validator);
+//console.log(validCard);
+
+document.getElementById("ingresoTarjeta").style.display="none";
+document.getElementById("validacionTarjeta").style.display="none";
+document.getElementById("bienvenida").style.display="none";
+document.getElementById("msj1").style.display="none";
+
+
+//Botón Reservación
+document.getElementById("reservaciones").addEventListener("click", ()=>{
+    
+    document.getElementById("bienvenida").style.display="block";
+    document.getElementById("bienvenida1").style.display="none";
+   
+    
+});
+//Botón Menor de Edad
+document.getElementById("btnMenorDeEdad").addEventListener("click", ()=>{
+    
+    document.getElementById("bienvenida1").style.display="none";
+    document.getElementById("bienvenida").style.display="none";
+    document.getElementById("msj1").style.display="block";
+    document.getElementById("msjMenorDeEdad").innerHTML = "Aún no puedes hacer reservaciones";
+});
+
+//Botón Mayor de edad
+
+document.getElementById("btnMayorDeEdad").addEventListener("click", ()=>{
+    document.getElementById("ingresoTarjeta").style.display="block";
+    document.getElementById("bienvenida").style.display="none";
+    document.getElementById("bienvenida1").style.display="none";
+});
+
+
+//Botón validar tarjeta
+
+document.getElementById("btnValidar").addEventListener("click", ()=>{
+    
+    document.getElementById("ingresoTarjeta").style.display="none";
+    document.getElementById("bienvenida1").style.display="none";
+    document.getElementById("bienvenida").style.display="none";
+    document.getElementById("validacionTarjeta").style.display="block";
+    //document.getElementById("cardNumberAsterisco").innerHTML = numeroTarjeta;
+
+    
+    const numeroTarjeta = document.getElementById("ingresoCardNumber").value;
+    
+    let estadoTarjeta = true;
+    let asterisco;
+    //4557880967583844
+    
+
+    estadoTarjeta = validator.isValid(numeroTarjeta);
+
+    if (numeroTarjeta.length == 0 ) {
+        alert("Debes llenar el campo");
+         document.getElementById("validacionTarjeta").style.display="none";
+         document.getElementById("ingresoTarjeta").style.display="block";
+      }
+
+    else{
+
+        if (estadoTarjeta == false) {
+        asterisco= validator.maskify(numeroTarjeta);
+        document.getElementById("cardNumberAsterisco").innerHTML ="NO VALIDO";
+        document.getElementById("numeroAsterisco").innerHTML =asterisco;
+        }
+  
+        else {
+            asterisco= validator.maskify(numeroTarjeta);
+            document.getElementById("cardNumberAsterisco").innerHTML = asterisco;
+        }
+    }
+
+    
+    
+
+    
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,45 @@
 const validator = {
-  // ...
+  isValid : (cardNumber)=>{
+      let reverseNum = Array.from(cardNumber).reverse();
+      let suma = 0;
+      let estado = true; //estado = 0 significa que el dato es valido
+      for (let i = 0; i < reverseNum.length; i++) {
+      
+        // Verificar si la posición es par
+        if ((i + 1) % 2 == 0) {
+            reverseNum[i] = reverseNum[i] * 2;
+            // Si es >= 10, usar truco del 9.
+            if (reverseNum[i] > 9) {
+            reverseNum[i] = reverseNum[i] - 9;
+            }
+        }
+        suma += parseInt(reverseNum[i]);
+      }
+        
+      if (suma % 10 == 0 ){
+        estado == true; //estado = 0 Si el dato es válido
+      }
+      else {
+        estado = false; //estado = 2 Si el dato es no válido
+      }    
+      
+      
+      return estado;
+  },
+
+  maskify : (cardNumber)=>{
+      let card = Array.from(cardNumber);
+        
+        for (let i = 0; i <=card.length; i++) {
+            if (i<card.length-4){
+                card[i]="#";
+            }
+    
+        }
+    
+        return card.join("");
+  }
+
 };
 
 export default validator;
