b3a69514a5c3f66f1e072dba1572d3992dd5e8f1
Milagros932 <milinr29@gmail.com>
2020-02-21 11:16:42 -0500

    subiendo archivo con la funcionalidad y los estilos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+//4083952015263
 import validator from './validator.js';
 
-console.log(validator);
+const btnValidar = document.getElementById('btn');
+const primeraVista = document.getElementById("primeraparte");
+const segundaVista = document.getElementById("segundaparte");
+
+segundaVista.style.display = 'none';
+//document.addEventListener("DOMContentLoaded", function () {
+btnValidar.addEventListener('click', () => {
+let numeroTarjeta = document.getElementById("tarjeta").value;
+let name = document.getElementById('nombre').value;
+
+//Validar el input del numero de tarjeta
+const expresion = /[^0-9]/;
+
+if(numeroTarjeta === "") {
+
+  document.getElementById("mensaje1").innerHTML = 'Este campo es obligatorio';
+} else if (expresion.test(numeroTarjeta)) {
+  document.getElementById("mensaje1").innerHTML= 'Debe ingresar solo 16 numeros';
+} else {
+
+//Algoritmo de luhm
+//if (numeroTarjeta.length==16) {
+
+  //const mensajito = document.getElementById('mensaje');
+  //mensajito.innerHTML = resultado;
+ const resultado = validator.isValid(numeroTarjeta);
+ const ocultar = validator.maskify(numeroTarjeta);
+
+    if (resultado==true){
+      primeraVista.style.display = 'none';
+      segundaVista.style.display = 'block';
+       const mensajeResultadoValido = document.getElementById("mensaje2");
+       mensajeResultadoValido.innerHTML= (name+'tu tarjeta'+ ocultar + ' '+'es valida');
+    }else {
+        const mensajeResultadoInvalido = document.getElementById("mensaje1");
+       mensajeResultadoInvalido.innerHTML= (name+'tu tarjeta'+ ' '+ocultar + ' '+'es invalida'+' '+'no se ejecuto tu pago');
+     document.getElementById("mensaje1").style.display = "block";
+            }
+//}else {
+  //alert('no valido');
+//}
+}
+});
+
+//});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,61 @@
-const validator = {
-  // ...
+
+const validator={
+isValid : (numeroTarjeta)=>{
+  //if (numeroTarjeta.length==16) {
+    let array=[];
+    for(let i = numeroTarjeta.length-1; i>=0; i--){
+      array= array + numeroTarjeta[i];
+    }
+
+    let array2=[];
+      for (let i = 1, l = array.length; i < l; i=i+2) {
+        array2.push(parseInt(array[i]*2));
+      }
+
+      let   posicion=0;
+      let array3=[];
+     for (let i =0; (array.length-1)/2 >i;i++){
+         if (array2[i]>9){
+       posicion   =(parseInt(array2[i])%10)+Math.floor(parseInt(array2[i])/10);
+       array3.push(parseInt(posicion.toString()));
+        }else{
+          posicion =array3.push(parseInt(array2[i]));
+            }
+      }
+
+      for (let i = 0; i <  array.length; i=i+2) {
+        array3.push(parseInt(array[i]));
+      }
+
+      let sumaarray = 0;
+      for (let i = 0; i < array3.length; i++){
+        sumaarray=sumaarray+parseInt(array3[i]);
+      }
+
+      if (sumaarray%10==0){
+        return true
+      } else{
+        return false
+    }
+
+},
+
+maskify : (numeroTarjeta)=>{
+
+  let array= numeroTarjeta.split('')
+
+
+  let arraymask=''
+  for (let i = 0; i < array.length; i++){
+    if ( i < (array.length-4) ){
+    arraymask+= '#' ;
+    }else{
+    arraymask+=numeroTarjeta[i] ;
+    }
+  }
+  return arraymask;
+}
+
 };
 
-export default validator;
+export default validator ;
