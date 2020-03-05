5a695be43cfadbf20666898b0fcb92d3d1fb76cc
rosterry58 <terrycajorosmery@gmail.com>
2020-03-05 09:46:25 -0500

    subiendo cambios en readme, html, javascript y css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import validator from './validator.js';
 const btn1 = document.getElementById('btn1'); // BOTÓN FORMULARIO 'inicio'
     btn1.addEventListener('click', () => {
     const email = document.getElementById('email').value;
-        if (email == "") {
+        if (email === '') {
             document.getElementById('email').focus();
         } else {
         document.getElementById('email').focus();
@@ -15,24 +15,37 @@ const btn1 = document.getElementById('btn1'); // BOTÓN FORMULARIO 'inicio'
 const btn2 = document.getElementById('btn2'); // BOTÓN FORMULARIO 'segundo'
     btn2.addEventListener('click', () => {
 
-    const numeroTarjeta = document.getElementById('creditCardNumber');
-    const isValidCardNumber = validator.isValid(numeroTarjeta.value);
+    const creditCardNumber = document.getElementById('creditCardNumber').value;
+    const nameCliente = document.getElementById('nameCliente').value;
+    
+    if (creditCardNumber === '' && nameCliente === '') {
+        document.getElementById('status1').innerHTML= 'Ingresar datos requeridos';
+        } else {
+        const numeroTarjeta = document.getElementById('creditCardNumber');
+        const isValidCardNumber = validator.isValid(numeroTarjeta.value);
 
-    const numeroTarjetaEncriptado = validator.maskify(numeroTarjeta.value);
-    numeroTarjeta.value = numeroTarjetaEncriptado;
+        const numeroTarjetaEncriptado = validator.maskify(numeroTarjeta.value);
+        numeroTarjeta.value = numeroTarjetaEncriptado;
+            
+        if (isValidCardNumber === true) {
+            document.getElementById('tarjeta válida').innerHTML= 'Tarjeta ingresada válida';
+            document.getElementById('status1').style.display='none';
+            document.getElementById('status2').style.display='block';
+            setTimeout('document.location.reload()',20000);
+            } else {
+            document.getElementById('tarjeta inválida').innerHTML= 'Tarjeta ingresada inválida';
+            document.getElementById('status1').style.display='none';
+            document.getElementById('status3').style.display='block';
+            setTimeout('document.location.reload()',10000);
+            }
+    
+        }
 
-    const status1 = document.getElementById('status1');
-    if (isValidCardNumber === true) {
-        status1.innerHTML = 'Tarjeta válida';
-    }
-    const status2 = document.getElementById('status2');
-    if (isValidCardNumber === false) {
-        status2.innerHTML = 'Tarjeta inválida';
-    }
 
-});
+    });
 
 
 
 
 
+    
