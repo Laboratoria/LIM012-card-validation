c1b5987a308a68fa1ccdad4f21cb196005716679
DianaMS <dmartinezs@uni.pe>
2020-08-09 04:06:08 -0500

    evento para validación de numero de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import validator from './validator.js';
+import validator from './validator.js';
 
 const page1 = document.querySelector('#page1');
 const page2 = document.querySelector('#page2');
@@ -10,6 +10,7 @@ const foneInput = form.querySelector('#fone-input');
 const dniInput = form.querySelector('#dni-input');
 // const button = form.getElementsByTagName('button')[0];
 const numCard = document.querySelector('.numCard');
+const textResult = document.querySelector('.text-result');
 const twoButtons = document.querySelector('.two-buttons');
 const leftButton = twoButtons.querySelector('.fa-arrow-circle-left');
 const rightButton = twoButtons.querySelector('.fa-arrow-circle-right');
@@ -26,8 +27,20 @@ form.addEventListener('submit', (e) => {
 leftButton.addEventListener('click', () => {
   page1.classList.remove('hide');
   page2.classList.add('hide');
-})
+});
 
+rightButton.addEventListener('click', () => {
+  const numberCard = numCard.value;
+  const regNum = /^([0-9])*$/;
 
+  if (!regNum.test(numberCard)) {
+    textResult.textContent = 'No está permitido ingresar letras'
+  }
+
+  if (validator.isValid(numberCard)) {
+    console.log('tu tarjeta es válida')
+    textResult.textContent = '';
+  }
+});
 // console.log(validator);
 // 4557880573586785
