331e07a38a39fb73143b34a5df6245167d7efb83
DianaMS <dmartinezs@uni.pe>
2020-08-09 03:34:51 -0500

    funcionalidad validación de formulario

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ const page1 = document.querySelector('#page1');
 const page2 = document.querySelector('#page2');
 const page3 = document.querySelector('#page3');
 const form = document.querySelector('#login-form');
-// const nameInput = form.querySelector('#name-input');
-// const emailInput = form.querySelector('#email-input');
-// const foneInput = form.querySelector('#fone-input');
-// const dniInput = form.querySelector('#dni-input');
-const button = form.getElementsByTagName('button')[0];
-// const numCard = document.querySelector('.numCard');
-// const twoButtons = document.querySelector('.two-buttons');
-// const leftButton = twoButtons.querySelector('.fa-arrow-circle-left');
-// const rightButton = twoButtons.querySelector('.fa-arrow-circle-right');
+const nameInput = form.querySelector('#name-input');
+const emailInput = form.querySelector('#email-input');
+const foneInput = form.querySelector('#fone-input');
+const dniInput = form.querySelector('#dni-input');
+// const button = form.getElementsByTagName('button')[0];
+const numCard = document.querySelector('.numCard');
+const twoButtons = document.querySelector('.two-buttons');
+const leftButton = twoButtons.querySelector('.fa-arrow-circle-left');
+const rightButton = twoButtons.querySelector('.fa-arrow-circle-right');
 page2.classList.add('hide');
 page3.classList.add('hide');
 
-button.addEventListener('click', (e) => {
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   page1.classList.add('hide');
   page2.classList.remove('hide');
 });
+
+leftButton.addEventListener('click', () => {
+  page1.classList.remove('hide');
+  page2.classList.add('hide');
+})
+
 
 // console.log(validator);
 // 4557880573586785
